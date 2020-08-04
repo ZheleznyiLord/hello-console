51f8c6ada4073c97b3aea09b923ee5ec84e56cc2
Moskalov Vadim <vad008@ukr.net>
2020-08-04 11:35:34 +0300

    create count work days

--- Program.cs
+++ Program.cs
@@ -6,13 +6,38 @@ namespace hello_console
     {
         public static void Main(string[] args)
         {
-            var message = getMessage("World");
-            Console.WriteLine(message);
+            DateTime[] dates = { new DateTime(2020, 2, 2), new DateTime(2020, 2, 3) };
+            Console.WriteLine(countWorkDays(new DateTime(2020, 2, 4), new DateTime(2020, 2, 1), dates));
+
         }
 
-        private static string getMessage(string name)
+
+
+        private static int countWorkDays(DateTime start, DateTime end, DateTime[] weekEnd)
         {
-            return "Hello " + name;
+            int count;
+            if (start > end)
+            {
+                throw new Exception("Start date must be earlier end date");
+            }
+            else
+            {
+                count = end.Day - start.Day + 1;
+                foreach (var day in weekEnd)
+                {
+                    if (day >= start && day <= end)
+                    {
+                        if (count == 0)
+                        {
+                            throw new Exception("Weekends must be not more then work days");
+                        }
+                        count--;
+                    }
+                }
+            }
+
+            return count;
         }
+
     }
 }
